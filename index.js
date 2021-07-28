fd1269045b8a96e81473ad96a51710ed69463315
Sam Hollyer <rsamhollyer@gmail.com>
2021-07-28 15:19:20 -0500

    finished base code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const { passWords, passCharacters } = require('./lib/diceware');
+#!/usr/bin/env node
+const { log } = console;
+const program = require('commander');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
+const savePassword = require('./lib/savepassword');
+const generatePassword = require('./lib/generatepassword');
+
+program.version('1.0.0').description('Simple Password Generator');
+
+program
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-s, --save', 'save password to passwords.txt')
+  .option('-asc, --ascii', 'created password is ascii characters only')
+  .parse();
+console.log(program.opts());
+const { length, save, isAscii } = program.opts();
+
+// Get generated password
+const generatedPassword = generatePassword(length, isAscii);
+
+// Save to file
+if (save) {
+  savePassword(generatedPassword);
+}
+
+// Copy to clipboard
+clipboardy.writeSync(generatedPassword);
+
+// Output generated password
+log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
+log(chalk.yellow('Password copied to clipboard'));
